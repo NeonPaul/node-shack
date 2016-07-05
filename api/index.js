bfd85fcc70d6102c3e61751f6ceb0e6664776635
NeonPaul <paulk@deskgen.com>
2016-07-05 16:45:54 +0100

    Add JWT

--- api/index.js
+++ api/index.js
@@ -1,3 +1,5 @@
+var jwt = require('jsonwebtoken');
+
 module.exports = function(resource, req, res){
   var token = req.headers['Authorization'];
   token = token && token.match(/Bearer ([^$]+)$/i)
@@ -8,25 +10,32 @@ module.exports = function(resource, req, res){
     method: req.method
   }
   res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify(request, null, 4));
 
-  /*
-   * If resource === auth
-   *   switch(method){
-   *     case 'POST':
-   *       // Lookup user
-   *       // Create token if found
-   *       // Return with 201
-   *       // else return with 401
-   *       break;
-   *     case 'GET':
-   *       // Validate token and return user with 200
-   *       // else return with 401
-   *       break;
-   *     default:
-   *       // send 405 method not allowed
-   *   }
-   *
-   * 
-   */
+  if(resource.indexOf('auth') === 0){
+    switch(req.method){
+      case 'POST':
+        let token = jwt.sign({ foo: 'bar' }, process.env.JWT_SECRET);
+        res.status(201);
+        // Lookup user
+        // Create token if found
+        // Return with 201
+        // else return with 401
+        break;
+      case 'GET':
+        let decoded;
+        if(decoded = jwt.verify(token, process.env.JWT_SECRET)){
+          res.status(200).end(JSON.stringify(decoded));
+        }else{
+          res.status(401).end('{ "message": "Wrong auth." }');
+        }
+        // Validate token and return user with 200
+        // else return with 401
+        break;
+      default:
+        res.status(405).end(`{ "message": "Only POST or GET accepted." }`);
+        break;
+    }
+  }else{
+    res.end(JSON.stringify(request, null, 4));
+  }
 }
