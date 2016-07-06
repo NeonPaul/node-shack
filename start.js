a490b8a01fd4a4380e8b7425dca32ebd5e4ac70f
NeonPaul <paulk@deskgen.com>
2016-07-06 14:20:44 +0100

    More logs

--- start.js
+++ start.js
@@ -18,6 +18,7 @@ cluster.on('disconnect', function(worker) {
 });
 
 if (cluster.isMaster) {
+  console.log(`PID ${process.pid}`);
   const workerCount = process.env.NODE_CLUSTER_WORKERS || 4;
   console.log(`Starting ${workerCount} workers...`);
   for (let i = 0; i < workerCount; i++) {
