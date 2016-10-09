1ae8897c4c9f68827736640455cf69fd11881abd
Paul Kiddle <neonpaul@gmail.com>
2016-10-09 13:09:33 +0100

    convert to express

--- app.js
+++ app.js
@@ -1,46 +1,26 @@
-const http         = require('http'),
-      fs           = require('fs'),
-      path         = require('path'),
-      contentTypes = require('./utils/content-types'),
-      sysInfo      = require('./utils/sys-info'),
-      env          = process.env;
+var sysInfo = require('./utils/sys-info')
+var env     = process.env
+var express = require('express')
 
-let server = http.createServer(function (req, res) {
-  let match;
-  let url = req.url;
-  if (url == '/') {
-    url += 'index.html';
+
+var app = express()
+
+// IMPORTANT: Your application HAS to respond to GET /health with status 200
+//            for OpenShift health monitoring
+app.get('/health', function(req, res){
+  res.sendStatus(200)
+})
+app.get('/info/:type', function(req, res, next){
+  var info = sysInfo[req.params.type]
+  if(info){
+    res.set('Cache-Control', 'no-cache, no-store')
+    res.json(info())
+  }else{
+    next()
   }
+})
+app.use(express.static(__dirname + '/static'))
 
-  // IMPORTANT: Your application HAS to respond to GET /health with status 200
-  //            for OpenShift health monitoring
-
-  if (url == '/health') {
-    res.writeHead(200);
-    res.end();
-  } else if (url == '/info/gen' || url == '/info/poll') {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Cache-Control', 'no-cache, no-store');
-    res.end(JSON.stringify(sysInfo[url.slice(6)]()));
-  } else if (match = url.match(/^\/api(?:\/([^$]*))?$/)) {
-    require('./api')(match[1] || '', req, res);
-  } else {
-    fs.readFile('./static' + url, function (err, data) {
-      if (err) {
-        res.writeHead(404);
-        res.end('Not found');
-      } else {
-        let ext = path.extname(url).slice(1);
-        res.setHeader('Content-Type', contentTypes[ext]);
-        if (ext === 'html') {
-          res.setHeader('Cache-Control', 'no-cache, no-store');
-        }
-        res.end(data);
-      }
-    });
-  }
-});
-
-server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
-  console.log(`Application worker ${process.pid} started...`);
+app.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
+  console.log(`Application worker ${process.pid} started...`)
 });
