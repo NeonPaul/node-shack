60092ee0c6b76b6022bc9784af74fd748094abcd
Paul Kiddle <neonpaul@gmail.com>
2017-11-13 21:03:10 +0000

    update post action

--- src/store/posts/actions.js
+++ src/store/posts/actions.js
@@ -11,8 +11,10 @@ const port = process.env.PORT || 3000
 const url = BROWSER ? '' : prot + '://' + host + ':' + port
 
 export const ADD_POSTS = 'add posts'
+export const UPDATE_POST = 'update post'
 
 export const addPosts = (payload) => ({ type: ADD_POSTS, payload })
+export const updatePostOk = (payload) => ({ type: UPDATE_POST, payload })
 export const fetchPosts = () => (dispatch, getState) => fetch(url + '/api/posts/', {
   headers: {
     'authorization': 'Bearer ' + getToken(getState())
@@ -26,3 +28,11 @@ export const createPost = content => (dispatch, getState) => fetch(url + '/api/p
   method: 'POST',
   body: JSON.stringify({ content })
 }).then(res => res.json()).then(json => dispatch(addPosts([json, ...getPosts(getState())])))
+export const updatePost = (id, content) => (dispatch, getState) => fetch(url + '/api/posts/' + id, {
+  headers: {
+    'authorization': 'Bearer ' + getToken(getState()),
+    'content-type': 'application/json'
+  },
+  method: 'MERGE',
+  body: JSON.stringify({ content })
+}).then(res => res.json()).then(json => dispatch(updatePostOk(json)))
--- src/store/posts/reducer.js
+++ src/store/posts/reducer.js
@@ -1,9 +1,11 @@
-import { ADD_POSTS } from './actions'
+import { ADD_POSTS, UPDATE_POST } from './actions'
 
-export default function (state = {}, action) {
+export default function (state = [], action) {
   switch (action.type) {
     case ADD_POSTS:
       return action.payload
+    case UPDATE_POST:
+      return state.map(post => post.id === action.payload.id ? action.payload : post)
     default:
       return state
   }
