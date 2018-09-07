6d9a0182a6178aa8fd8f6ec94f162d4056cbf7c4
Paul <p.kiddle@nhm.ac.uk>
2018-09-07 16:50:15 +0100

    Add service worker config

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,20 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /service-worker\/index\.js/,
+        use: [{
+          loader: 'service-worker-loader',
+          options: {
+            filename: 'sw.js'
+          }
+        }]
+      },
+      {
         test: /\.js$/,
-        exclude: /node_modules/,
+        exclude: [
+          /node_modules/,
+          /service-worker\/index\.js/,
+        ],
         use: {
           loader: "babel-loader"
         }
