53ae6090ef4ad80469be3e981386aad4c1a016d6
Paul <p.kiddle@nhm.ac.uk>
2018-09-08 15:39:51 +0100

    Login link flow

--- server/router/login.js
+++ server/router/login.js
@@ -9,6 +9,33 @@ const pool = require('../db');
 const passwordHash = new PasswordHash();
 const route = new exporess.Router();
 const JWT_SECRET = getenv("JWT_SECRET", "big secret");
+const getUser = async user => {
+  const [ results ] = await pool.query(sql`SELECT * FROM users WHERE email=${user} OR user=${user}`);
+
+  if (!results || !results[0]) {
+    throw new Error(`No user ${ user } found`);
+  }
+
+  return results[0];
+}
+
+const login = (result, req, res) => {
+    // TODO: Use another value for token, not email
+    const token = jwt.sign(
+      { email: result.email },
+      JWT_SECRET
+    );
+
+    res.cookie('token', token, { maxAge: 365 * 24 * 3600 * 1000 });
+    req.found = true;
+    // TODO: Share this logic with auth.js
+    // or do a redirect and have index pick up the correct state
+    res.state.user = {
+      user: result.user,
+      email: result.email,
+    }
+    res.location('/');
+}
 
 route.post('/',  async (req, res, next) => {
   try {
@@ -18,34 +45,47 @@ route.post('/',  async (req, res, next) => {
       throw new Error('User or password is empty');
     }
 
-    const [ results ] = await pool.query(sql`SELECT * FROM users WHERE email=${user} OR user=${user}`);
+    const result = await getUser();
 
-    if (!results || !results[0]) {
-      throw new Error(`No user ${ user } found`);
-    }
-
-    if (!passwordHash.checkPassword(password, results[0].password)) {
+    if (!passwordHash.checkPassword(password, result.password)) {
       throw new Error('Password incorrect');
     }
 
-    // TODO: Use another value for token, not email
-    const token = jwt.sign(
-      { email: results[0].email },
-      JWT_SECRET
-    );
-
-    res.cookie('token', token, { maxAge: 365 * 24 * 3600 * 1000 });
-    req.found = true;
-    // TODO: Share this logic with auth.js
-    // or do a redirect and have index pick up the correct state
-    res.state.user = {
-      user: results[0].user,
-      email: results[0].email,
-    }
-    res.location('/');
+    login(result, req, res);
     next();
   } catch(e) {
     next(e);
+  }
+});
+
+route.post('/reset', async (req, res, next) => {
+  try {
+    const { user } = req.body;
+
+    const result = await getUser(user);
+
+    const token = jwt.sign({
+      user: result.user
+    }, result.password, { expiresIn: '1d' });
+
+    res.send(`<a href="/login/reset?token=${token}">Reset</a>`);
+  }catch(e) {
+    next(e)
+  }
+});
+
+route.get('/reset', async (req, res, next) => {
+  try {
+    const token = jwt.decode(req.query.token);
+
+    const user = await getUser(token.user);
+
+    jwt.verify(req.query.token, user.password);
+
+    login(user, req, res);
+    next();
+  }catch(e) {
+    next(e)
   }
 });
 
--- src/components/login.js
+++ src/components/login.js
@@ -8,10 +8,17 @@ export default class Login extends React.Component {
       <div className='Login__content'>
         <h1 className='Login__title'>Log in</h1>
         <Form action="/login" method="post">
-          <Input name='user' autoFocus>Email</Input>
+          <Input name='user' autoFocus>Username/Email</Input>
           <Input type='password' name='password'>Password</Input>
           <p className='Login__control'>
             <button>Login</button>
+          </p>
+        </Form>
+
+        <Form action="/login/reset" method="post">
+          <Input name='user'>Username/Email</Input>
+          <p className='Login__control'>
+            <button>Reset password</button>
           </p>
         </Form>
       </div>
